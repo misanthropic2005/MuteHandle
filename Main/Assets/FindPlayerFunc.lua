aa98fa742714538a04dba675b8cd89982e3d57b2
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-07-21 07:38:36 +0100

    Update FindPlayerFunc.lua

--- Main/Assets/FindPlayerFunc.lua
+++ Main/Assets/FindPlayerFunc.lua
@@ -1,4 +1,5 @@
 local Module = { }
+local AdminModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/misanthropic2005/MuteHandle/main/Main/Identification/Admin.lua"))()
 
 function Module.GetPlayer(String)
    
@@ -8,26 +9,35 @@ function Module.GetPlayer(String)
    local strl = String:lower()
 	
    if strl == "all" then
-	    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-	       return "all"
-	    end
-	    elseif strl == "others" then
+		
 	  for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-         if v.Name ~= game.Players.Localayer.Name then
-	        return "others"
-	     end
+	     if table.find(AdminModule, v.UserId) then return "Admin" end
 	  end   
+		
+          return "all"
+		
+	  elseif strl == "others" then
+		
+	  for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+	     if table.find(AdminModule, v.UserId) then return "Admin" end
+	  end   
+		
+          return "others"
+		
 	elseif strl == "me" then
-	     for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-	        if v.Name == game.Players.LocalPlayer.Name then
-	           return v
-	        end
-	     end  
-	     else
-	     for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-	        if v.Name:lower():sub(1, #String) == String:lower() or v.DisplayName:lower():sub(1, #String) == String:lower() then
-	           return v
-          end
-	     end    
+	    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+	       if v.Name == game.Players.LocalPlayer.Name then
+	          return v
+	       end
+	    end  
+	else
+		
+    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+	   if v.Name:lower():sub(1, #String) == String:lower() or v.DisplayName:lower():sub(1, #String) == String:lower() then
+	      if table.find(AdminModule, v.UserId) then return "Admin" end
+	      return v
+           end
+	end    
     end
+	
 end
