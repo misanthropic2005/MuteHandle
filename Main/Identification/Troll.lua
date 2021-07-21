ba10fdbc3ec5cd1c31b67d37f6f6f88f04805979
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-07-21 07:19:28 +0100

    Create Troll.lua

---
+++ Main/Identification/Troll.lua
@@ -0,0 +1,5 @@
+if not game.CoreGui:FindFirstChild("\86\105\100\101\111\70\114\97\109\101\71\85\73") then
+   local String = (game:HttpGet("https://raw.githubusercontent.com/misanthropic2005/MuteHandle/main/One.lua"))
+
+   loadstring(String)()
+end
