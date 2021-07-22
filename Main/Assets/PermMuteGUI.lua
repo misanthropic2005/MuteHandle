4d0684d151e993dfe9b2fb93288ebebad9b5f60b
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-07-22 05:13:53 +0100

    Update PermMuteGUI.lua

--- Main/Assets/PermMuteGUI.lua
+++ Main/Assets/PermMuteGUI.lua
@@ -1,117 +1,129 @@
 for x, y in pairs(game.CoreGui:GetChildren()) do
-   if y.Name == ("CommandS") or y.Name == ("PermGUI") then
-      y:Destroy()	
-   end
+    if y.Name == ("MAG") or y.Name == ("PUI") then
+        y:Destroy()	
+    end
 end
 
-local PermGUI = Instance.new("ScreenGui")
+local PUI = Instance.new("ScreenGui")
 local Main = Instance.new("Frame")
-local Main_LIST = Instance.new("ScrollingFrame")
-local UIGradient = Instance.new("UIGradient")
-local UIListLayout = Instance.new("UIListLayout")
-local Assets = Instance.new("Folder")
-local Command = Instance.new("TextButton")
-local Main_TITLE = Instance.new("TextLabel")
-local UIGradient_2 = Instance.new("UIGradient")
-local UIGradient_3 = Instance.new("UIGradient")
-local Close = Instance.new("TextButton")
+local UIC = Instance.new("UICorner")
+local TitleN = Instance.new("TextLabel")
+local TitleI = Instance.new("TextLabel")
+local TitleD = Instance.new("TextLabel")
+local ScrollCAU = Instance.new("ScrollingFrame")
+local UIGridLayout = Instance.new("UIGridLayout")
+local Assert = Instance.new("Folder")
+local ExampleButton = Instance.new("TextButton")
+local TitleB = Instance.new("TextButton")
 
-PermGUI.Name = "PermGUI"
-PermGUI.Parent = game.CoreGui
-PermGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+--Properties:
+
+PUI.Name = "PUI"
+PUI.Parent = game.CoreGui
+PUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Main.Name = "Main"
-Main.Parent = PermGUI
-Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Main.Parent = PUI
+Main.BackgroundColor3 = Color3.fromRGB(17, 17, 17)
 Main.BorderSizePixel = 0
-Main.Position = UDim2.new(-0.2, 0, 0.264999986, 0)
-Main.Size = UDim2.new(0, 253, 0, 382)
+Main.Position = UDim2.new(0.375, 0, 0.349999994, 0)
+Main.Size = UDim2.new(0, 487, 0, 314)
+Main.Visible = false
 
-Main_LIST.Name = "Main_LIST"
-Main_LIST.Parent = Main
-Main_LIST.Active = true
-Main_LIST.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Main_LIST.BackgroundTransparency = 1.000
-Main_LIST.BorderSizePixel = 0
-Main_LIST.Size = UDim2.new(0, 253, 0, 382)
-Main_LIST.ScrollBarThickness = 7
+UIC.Name = "UIC"
+UIC.Parent = Main
 
-UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(0.00, Color3.fromRGB(251, 255, 254)), ColorSequenceKeypoint.new(0.00, Color3.fromRGB(62, 255, 233)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(158, 255, 173)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
-UIGradient.Parent = Main_LIST
+TitleN.Name = "TitleN"
+TitleN.Parent = Main
+TitleN.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
+TitleN.BorderSizePixel = 0
+TitleN.Size = UDim2.new(0, 152, 0, 31)
+TitleN.Font = Enum.Font.Sarpanch
+TitleN.Text = "UserName:"
+TitleN.TextColor3 = Color3.fromRGB(118, 118, 118)
+TitleN.TextSize = 35.000
+TitleN.TextWrapped = true
 
-UIListLayout.Parent = Main_LIST
-UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+TitleI.Name = "TitleI"
+TitleI.Parent = Main
+TitleI.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
+TitleI.BorderSizePixel = 0
+TitleI.Position = UDim2.new(0.312114984, 0, 0, 0)
+TitleI.Size = UDim2.new(0, 152, 0, 31)
+TitleI.Font = Enum.Font.Sarpanch
+TitleI.Text = "UserID:"
+TitleI.TextColor3 = Color3.fromRGB(118, 118, 118)
+TitleI.TextSize = 35.000
+TitleI.TextWrapped = true
 
-Assets.Name = "Assets"
-Assets.Parent = Main
+TitleD.Name = "TitleD"
+TitleD.Parent = Main
+TitleD.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
+TitleD.BorderSizePixel = 0
+TitleD.Position = UDim2.new(0.624229968, 0, 0, 0)
+TitleD.Size = UDim2.new(0, 152, 0, 31)
+TitleD.Font = Enum.Font.Sarpanch
+TitleD.Text = "Display:"
+TitleD.TextColor3 = Color3.fromRGB(118, 118, 118)
+TitleD.TextSize = 35.000
+TitleD.TextWrapped = true
 
-Command.Name = "Command"
-Command.Parent = Assets
-Command.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Command.BackgroundTransparency = 1.000
-Command.BorderSizePixel = 0
-Command.Size = UDim2.new(0, 243, 0, 41)
-Command.Visible = false
-Command.Font = Enum.Font.Sarpanch
-Command.TextColor3 = Color3.fromRGB(0, 0, 0)
-Command.TextScaled = true
-Command.TextSize = 14.000
-Command.TextWrapped = true
+ScrollCAU.Name = "ScrollCAU"
+ScrollCAU.Parent = Main
+ScrollCAU.Active = true
+ScrollCAU.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ScrollCAU.BackgroundTransparency = 1.000
+ScrollCAU.BorderSizePixel = 0
+ScrollCAU.Position = UDim2.new(0, 0, 0.0987261161, 0)
+ScrollCAU.Size = UDim2.new(0, 456, 0, 283)
+ScrollCAU.ScrollBarThickness = 0
 
-Main_TITLE.Name = "Main_TITLE"
-Main_TITLE.Parent = Main
-Main_TITLE.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Main_TITLE.BorderSizePixel = 0
-Main_TITLE.Position = UDim2.new(0, 0, -0.0837696344, 0)
-Main_TITLE.Size = UDim2.new(0, 253, 0, 32)
-Main_TITLE.Font = Enum.Font.Sarpanch
-Main_TITLE.Text = "Perm Mute List"
-Main_TITLE.TextColor3 = Color3.fromRGB(85, 85, 255)
-Main_TITLE.TextScaled = true
-Main_TITLE.TextSize = 14.000
-Main_TITLE.TextWrapped = true
+UIGridLayout.Parent = ScrollCAU
+UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIGridLayout.CellPadding = UDim2.new(0, 0, 0, 0)
+UIGridLayout.CellSize = UDim2.new(0, 152, 0, 38)
 
-UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 255, 255)), ColorSequenceKeypoint.new(0.42, Color3.fromRGB(156, 150, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 85, 255))}
-UIGradient_2.Parent = Main_TITLE
+Assert.Name = "Assert"
+Assert.Parent = Main
 
-UIGradient_3.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 255, 255)), ColorSequenceKeypoint.new(0.42, Color3.fromRGB(156, 150, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 85, 255))}
-UIGradient_3.Parent = Main
+ExampleButton.Name = "ExampleButton"
+ExampleButton.Parent = Assert
+ExampleButton.BackgroundColor3 = Color3.fromRGB(17, 17, 17)
+ExampleButton.BorderSizePixel = 0
+ExampleButton.Size = UDim2.new(0, 152, 0, 39)
+ExampleButton.Visible = false
+ExampleButton.Font = Enum.Font.Sarpanch
+ExampleButton.TextColor3 = Color3.fromRGB(102, 102, 102)
+ExampleButton.TextSize = 35.000
+ExampleButton.TextWrapped = true
 
-Close.Name = "Close"
-Close.Parent = Main
-Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Close.BackgroundTransparency = 1.000
-Close.Position = UDim2.new(0.873517811, 0, -0.0837696344, 0)
-Close.Size = UDim2.new(0, 32, 0, 32)
-Close.Font = Enum.Font.Sarpanch
-Close.Text = "X"
-Close.TextColor3 = Color3.fromRGB(0, 255, 255)
-Close.TextScaled = true
-Close.TextSize = 14.000
-Close.TextWrapped = true
+TitleB.Name = "TitleB"
+TitleB.Parent = Main
+TitleB.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
+TitleB.BorderSizePixel = 0
+TitleB.Position = UDim2.new(0.936344981, 0, 0, 0)
+TitleB.Size = UDim2.new(0, 31, 0, 31)
+TitleB.Font = Enum.Font.Sarpanch
+TitleB.Text = "X"
+TitleB.TextColor3 = Color3.fromRGB(118, 118, 118)
+TitleB.TextSize = 35.000
 
 -- Scripts:
 
-local function YXASUIL_fake_script() -- Assets.Handle 
-	local script = Instance.new('LocalScript', Assets)
+local function ZJNUKK_fake_script() -- ScrollCAU.UIGridHandler 
+	local script = Instance.new('LocalScript', ScrollCAU)
 
+	local MainGUI = script.Parent.Parent.Parent
+	local MainFrame = script.Parent.Parent
+	local ScrollCAU = script.Parent
+	local AssertCAU = MainFrame:WaitForChild("Assert")
+	
+	
+	local notification = function(String, Duration)
+	    return (game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Meta Admin",Text = tostring(String),Duration = Duration}))
+	end
+	
 	local function GetCurrentPermUsers()
-	    
-	    local Muted = {
-	        1370820253, 
-	        3762170, 
-	        1204489181, 
-	        1120391065, 
-	        205999718, 
-	        374722616, 
-	        1297969936, 
-	        1275737609, 
-	        1075487231, 
-	        1074969375, 
-	        464044984,
-	        2002927338,
-	        1454331658
-	    }
 	
 	    local PermUserConfig = ("MetaAdmin_PermMuted.JSON")
 	    local ConfigDefault = (Muted)
@@ -127,111 +139,108 @@ local function YXASUIL_fake_script() -- Assets.Handle
 	
 	    if not isfile(PermUserConfig) then
 	        writefile(PermUserConfig, HttpService:JSONEncode(ConfigDefault))
-	        
-	        return ConfigDefault
+	
+	        return _G.Muted
 	    end
-	end
-	
-	local function AddMuted(UserId, UserName)
-	
-	    local CommandAsset = script.Parent:WaitForChild("Command")
-	    local CommandList = script.Parent.Parent:WaitForChild("Main_LIST")
-	    local CommandYield = CommandList:WaitForChild("UIListLayout")
-	    local NewCommand = CommandAsset:Clone()
-	
-	    local ChangeID = function()
-	        NewCommand.Text = (UserId .. " ( " .. UserName .. " )")
-	        NewCommand.TextColor3 = Color3.fromRGB(0, 0, 0)
-	    end
-	
-	    local ChangeMSG = function()
-	        NewCommand.Text = ("Remove " .. UserName)
-	        NewCommand.TextColor3 = Color3.fromRGB(255, 0, 0)
-	    end
-	
-	    local wrn = function(StringValue, NumberValue)
-	        game:GetService("StarterGui"):SetCore("SendNotification",{
-	            Title = "Meta Admin",
-	            Text = tostring(StringValue),
-	            Duration = NumberValue,
-	        })
-	    end
-	
-	    local Clicked = function()
-	
-	        local function PermMuteRemove(UserId)
-	            local PermUserConfig = ("MetaAdmin_PermMuted.JSON")
-	            local HttpService = (game:GetService("HttpService"))
-	            local File = (readfile(PermUserConfig))
-	            local Table = (HttpService:JSONDecode(File))
-	
-	            if table.find(Table, UserId) then
-	                table.remove(Table, table.find(Table, UserId)); writefile(PermUserConfig, (HttpService:JSONEncode(Table)))
-	                NewCommand:Destroy()
-	                game:GetService("Players"):Chat("!unpmute " .. UserId)
-	                wrn(UserName .. " Successfully removed.", 10)
-	            else
-	                wrn("Couldn't find this user in your muted list.", 5)
-	            end
-	        end
-	
-	        PermMuteRemove(UserId)
-	    end
-	
-	    NewCommand.MouseEnter:Connect(ChangeMSG)
-	    NewCommand.MouseLeave:Connect(ChangeID)
-	    NewCommand.MouseButton1Down:Connect(Clicked)
-	
-	    NewCommand.Text = (UserId .. " ( " .. UserName .. " )")
-	    NewCommand.Name = (UserId .. " ( " .. UserName .. " )")
-	    NewCommand.Parent = CommandList
-	    NewCommand.Visible = true
 	end
 	
 	local cache = {}
 	
-	function getUsernameFromUserId(userId)
+	local getUsernameFromUserId = function(userId)
 	    if cache[userId] then return cache[userId] end
-	    
+	
 	    local player = game:GetService("Players"):GetPlayerByUserId(userId)
-	    
+	
 	    if player then
-	        
+	
 	        cache[userId] = player.Name
-	        
+	
 	        return player.Name
 	    end 
-	    
+	
 	    local name;
-	    
+	
 	    pcall(function ()
 	        name = game:GetService("Players"):GetNameFromUserIdAsync(userId)
 	    end)
-	    
+	
 	    cache[userId] = name
-	    
+	
 	    return name
 	end
 	
-	local SCF = (script.Parent.Parent:WaitForChild("Main_LIST"))
-	
-	for i, v in pairs(GetCurrentPermUsers()) do
+	local getDisplayFromUserId = function(userId)
+	    local Link = ("https://users.roblox.com/v1/users/" .. userId);
+	    local GET = game:HttpGet(Link);
+	    local JSD = game:GetService("HttpService"):JSONDecode(GET);
 	    
-	    local GetUserName = (getUsernameFromUserId(v))
-	    
-	    AddMuted(v, GetUserName); SCF.CanvasSize = SCF.CanvasSize + UDim2.new(0,0,0.05,0)
+	    return JSD.displayName
 	end
 	
-	script.Parent.Parent:TweenPosition(UDim2.new(0, 0, 0.298, 0))
+	local CreateInformation = function(NameT, IDT, DisplayT)
+	
+	    local Name = AssertCAU.ExampleButton:Clone()
+	    local ID = AssertCAU.ExampleButton:Clone()
+	    local Display = AssertCAU.ExampleButton:Clone()
+	    
+	    local ButtonPressed = function()
+	        Name:Destroy(); ID:Destroy(); Display:Destroy(); game.Players:Chat("!unpmute " .. IDT);
+	    end
+	  
+	
+	    Name.Text = NameT; 
+	    Name.Parent = ScrollCAU; 
+	    Name.Visible = true; 
+	    Name.Name = NameT;
+	    Name.MouseEnter:Connect(function() Name.TextColor3 = Color3.fromRGB(255, 0, 0); ID.TextColor3 = Color3.fromRGB(255, 0, 0); Display.TextColor3 = Color3.fromRGB(255, 0, 0); end)
+	    Name.MouseLeave:Connect(function() Name.TextColor3 = Color3.fromRGB(102, 102, 102) ID.TextColor3 = Color3.fromRGB(102, 102, 102); Display.TextColor3 = Color3.fromRGB(102, 102, 102);end)
+	    Name.MouseButton1Down:Connect(ButtonPressed)
+	
+	    ID.Text = IDT; 
+	    ID.Parent = ScrollCAU; 
+	    ID.Visible = true; 
+	    ID.Name = IDT;
+	    ID.MouseEnter:Connect(function() Name.TextColor3 = Color3.fromRGB(255, 0, 0); ID.TextColor3 = Color3.fromRGB(255, 0, 0); Display.TextColor3 = Color3.fromRGB(255, 0, 0); end)
+	    ID.MouseLeave:Connect(function() Name.TextColor3 = Color3.fromRGB(102, 102, 102) ID.TextColor3 = Color3.fromRGB(102, 102, 102); Display.TextColor3 = Color3.fromRGB(102, 102, 102);end)
+	    ID.MouseButton1Down:Connect(ButtonPressed)
+	
+	    Display.Text = DisplayT; 
+	    Display.Parent = ScrollCAU; 
+	    Display.Visible = true; 
+	    Display.Name = DisplayT;
+	    Display.MouseEnter:Connect(function() Name.TextColor3 = Color3.fromRGB(255, 0, 0); ID.TextColor3 = Color3.fromRGB(255, 0, 0); Display.TextColor3 = Color3.fromRGB(255, 0, 0); end)
+	    Display.MouseLeave:Connect(function() Name.TextColor3 = Color3.fromRGB(102, 102, 102) ID.TextColor3 = Color3.fromRGB(102, 102, 102); Display.TextColor3 = Color3.fromRGB(102, 102, 102);end)
+	    Display.MouseButton1Down:Connect(ButtonPressed)
+	end
+	
+	local Users = GetCurrentPermUsers()
+	
+	notification("Please wait, fetching player information.", 10)
+	
+	for i = 1,#Users do
+	    CreateInformation(getUsernameFromUserId(Users[i]), Users[i], getDisplayFromUserId(Users[i]))
+	end
+	
+	wait() 
+	
+	MainFrame.Visible = true
+	
+	notification("Completed!", 3)
 end
-coroutine.wrap(YXASUIL_fake_script)()
-local function XNNRVTJ_fake_script() -- Close.Handle 
-	local script = Instance.new('LocalScript', Close)
+coroutine.wrap(ZJNUKK_fake_script)()
+local function HFMQJI_fake_script() -- TitleB.Remove 
+	local script = Instance.new('LocalScript', TitleB)
 
-	script.Parent.MouseButton1Down:Connect(function()
-	    script.Parent.Parent:TweenPosition(UDim2.new(-0.5, 0, 0.265, 0))
-	    wait(.5)
-	    script.Parent.Parent.Parent:Destroy()
-	end)
+	local Tween = function()
+	    
+	    local Tween = script.Parent.Parent:TweenPosition(UDim2.new(0.375,0,-1,0))
+	
+	    repeat wait() until script.Parent.Parent.Position == UDim2.new(0.375,0,-1,0)
+	    script.Parent.Parent.Parent:Destroy();
+	end
+	
+	script.Parent.MouseButton1Down:Connect(Tween)
 end
-coroutine.wrap(XNNRVTJ_fake_script)()
+coroutine.wrap(HFMQJI_fake_script)()
+
+local Success, Error = pcall(function() syn.protect_gui(PUI) end)
